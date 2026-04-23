d1f9aa04b42e563f3fde3a899b1be089cd67983d
vibio <visbio.tamauniv@gmail.com>
2026-04-23 17:41:06 +0900

    fps確認

--- videocapture.py
+++ videocapture.py
@@ -1,33 +1,79 @@
 import time
-import pyautogui
 import cv2
 import numpy as np
+import mss
+import threading
 
 fps = 60
-cap_region = (0, 0, 1920, 1030)
+
+# mss用の指定（dict形式）
+# monitor = {"top": 0, "left": 0, "width": 1920, "height": 1030}
+monitor = {"top": 0, "left": 0, "width": 1280, "height": 720}
 
 def Startcapture(stop_event):
+    # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('capture.mp4', fourcc, fps, (1280, 720))
 
-    # 動画ライターを最初に作る
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter('capture.mp4', fourcc, fps, (1920, 1030))
+    sct = mss.mss()
 
     print("録画開始")
+    frame_count = 0
+    start_time = time.perf_counter()
 
     while not stop_event.is_set():
-        start = time.perf_counter()
+        # 高速キャプチャ
+        sct_img = sct.grab(monitor)
 
-        cap = pyautogui.screenshot(region=cap_region)
-        img = cv2.cvtColor(np.array(cap), cv2.COLOR_RGB2BGR)
-
-        # ★ここで即書き込み
+        # numpy配列に変換（BGRA → BGR）
+        img = np.array(sct_img)[:, :, :3]
+        
         out.write(img)
+        frame_count += 1
 
-        end = time.perf_counter()
+    
+    end_time = time.perf_counter()
 
-        if end - start < 1 / fps:
-            time.sleep(1 / fps - (end - start))
+    actual_fps = frame_count / (end_time - start_time)
+    print(actual_fps)
 
-    # 終了処理
+    # while not stop_event.is_set():
+    #     start = time.perf_counter()
+
+    #     # 高速キャプチャ
+    #     sct_img = sct.grab(monitor)
+
+    #     # numpy配列に変換（BGRA → BGR）
+    #     img = np.array(sct_img)[:, :, :3]
+
+    #     out.write(img)
+
+    #     end = time.perf_counter()
+
+    #     # FPS制御
+    #     elapsed = end - start
+    #     if elapsed < 1 / fps:
+    #         time.sleep(1 / fps - elapsed)
+
     out.release()
     print("録画終了")
+
+def get_ts():
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_id = 0
+
+    ts = frame_id / fps
+
+
+
+stop_event = threading.Event()
+capturethread = threading.Thread(target=Startcapture, args=(stop_event,))
+#録画開始
+capturethread.start()
+
+time.sleep(10)
+
+stop_event.set()
+
+# 終了待ち（重要）
+capturethread.join()
